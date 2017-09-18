fd3c7bebead2077b0ee4dc50cb32b6eb7e18e4ab
BIWhitfield <beniwhitfield@gmail.com>
2017-09-18 17:24:36 +0100

    [V3-3] Finish set up of webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'The Wire', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Friends', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'House Of Cards', color: 'Peru' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));

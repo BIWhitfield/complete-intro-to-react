f648e10e6630525f119a3ab84d659575c3549569
Ben <beniwhitfield@gmail.com>
2017-09-18 20:42:08 +0100

    [V3-4] Routing props state management (#3)
    * [V3-2] Set up tools (babel, webpack, lint, prettier)
    
    * [V3-3] Finish set up of webpack
    
    * [V3-4]

---
+++ js/Search.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
+
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTermChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;

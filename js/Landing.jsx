145db83fe8005a5bc68043327c61c4830c8c005f
BIWhitfield <beniwhitfield@gmail.com>
2017-09-28 15:52:10 +0100

    [Redux] add redux functionality

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,49 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import type { RouterHistory } from 'react-router-dom';
+import { setSearchTerm } from './actionCreators';
 
-const Landing = () => (
-  <div className="landing">
-    <h1>svideo</h1>
-    <input type="text" placeholder="Search" />
-    <Link to="/search">or Browse All</Link>
-  </div>
-);
+class Landing extends Component {
+  props: {
+    searchTerm: string,
+    handleSearchTermChange: Function,
+    history: RouterHistory
+  };
 
-export default Landing;
+  goToSearch = (e: SyntheticEvent) => {
+    e.preventDefault();
+    this.props.history.push('/search');
+  };
+
+  render() {
+    return (
+      <div className="landing">
+        <h1>svideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input
+            onChange={this.props.handleSearchTermChange}
+            value={this.props.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+          <Link to="/search">or Browse All</Link>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange: e => {
+    dispatch(setSearchTerm(e.target.value));
+  }
+});
+
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
